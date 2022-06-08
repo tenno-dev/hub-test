5331472ad5e30b48a7469ea531d2f16f1d0f6e96
Bitti09 <Bitti09@users.noreply.github.com>
2022-06-09 01:33:51 +0200

    add unocss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import 'virtual:uno.css'
+    //import '$lib/global.css'
+  </script>
+  
+  <svelte:head>
+    <title>Welcome</title>
+  </svelte:head>
+  
+  <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,36 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import Unocss from 'unocss/vite'
+import presetIcons from '@unocss/preset-icons'
+import presetTagify from '@unocss/preset-tagify'
+
+import { presetUno, presetAttributify } from 'unocss'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			plugins: [
+				Unocss({
+					presets: [
+						presetAttributify({ /* preset options */ }),
+						presetUno(),
+						presetTagify({ /* options */ }),
+						presetIcons({
+							extraProperties: {
+								'display': 'inline-block',
+								'vertical-align': 'middle',
+								'position': 'relative',
+
+								// ...
+							}
+						})
+						// ...custom presets
+					]
+				})
+			]
+
+		}
 	}
 };
 
